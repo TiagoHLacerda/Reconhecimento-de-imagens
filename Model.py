b0dfff49b4bd7f5eba56cf78bc93710ba2e7fcde
TiagoHLacerda <51682610+TiagoHLacerda@users.noreply.github.com>
2025-05-02 11:59:10 +0000

    Apresentação

--- Model.py
+++ Model.py
@@ -15,12 +15,13 @@ mean = 125.3
 std = 63.0
 
 def preprocess_user_image(image_file, mean, std):
-    img = Image.open(image_file).convert('RGB')
-    img = img.resize((32, 32))
+    original_img = Image.open(image_file).convert('RGB')  # Salva a original
+    img = original_img.resize((32, 32))  # Redimensiona só para o modelo
     img_array = np.array(img)
     img_array = (img_array - mean) / std
     img_array = np.expand_dims(img_array, axis=0)
-    return img_array, img
+    return img_array, original_img
+
 
 def classify_user_image(model, img_array):
     predictions = model.predict(img_array)
@@ -30,6 +31,19 @@ def classify_user_image(model, img_array):
 
 # Interface Streamlit
 st.title("🔍 Reconhecimento de Imagens - CIFAR-10")
+st.markdown("### Este modelo reconhece 10 categorias do CIFAR-10:")
+st.markdown("""
+- ✈️ Avião  
+- 🚗 Automóvel  
+- 🐦 Pássaro  
+- 🐱 Gato  
+- 🦌 Veado  
+- 🐶 Cachorro  
+- 🐸 Sapo  
+- 🐴 Cavalo  
+- 🚢 Navio  
+- 🚛 Caminhão
+""")
 
 uploaded_file = st.file_uploader("Envie uma imagem:", type=["jpg", "png", "jpeg"])
 
@@ -37,5 +51,6 @@ if uploaded_file is not None:
     img_array, original_img = preprocess_user_image(uploaded_file, mean, std)
     predicted_class = classify_user_image(model, img_array)
 
-    st.image(original_img, caption=f"Imagem enviada", use_column_width=True)
+    st.image(original_img, caption="Imagem enviada", use_container_width=True)
+
     st.success(f"🧠 Classe prevista: **{predicted_class}**")
